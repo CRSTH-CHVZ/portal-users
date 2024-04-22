36ab91ab2cbb0639e2bc28ab18e070b33468c22a
Cristhian-Chavez <crsth.1990@fpuna.edu.py>
2024-04-22 04:58:38 -0400

    Generate excel report

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import {useEffect,} from "react";
 import {getAlbums, getAllUsers} from "@/app/services/services";
 import {useAlbumsStore, useUsersStore} from "@/app/store/store";
 import CardUser from "@/app/(components)/CardUser";
+import {exportExcel} from "@/app/(components)/exportExcel";
 
 
 export default function Home() {
@@ -19,24 +20,25 @@ export default function Home() {
             })
     }, []);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     {/*   todo add navbar*/}
-     {/*   todo add state manager*/}
-     <div>Portal</div>
-        {
-            users?.length > 0 && (
-                <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-                    {
-                        users?.map((user: any) => (
-                            <CardUser
-                                key={user.id}
-                                {...user}
-                            />
-                        ))
-                    }
-                </div>
-            )
-        }
-    </main>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+          {/*   todo add navbar*/}
+          {/*   todo add state manager*/}
+          <div>Portal</div>
+          <div onClick={ async  () => { exportExcel(users) }}>Exportar Excel</div>
+          {
+              users?.length > 0 && (
+                  <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+                      {
+                          users?.map((user: any) => (
+                              <CardUser
+                                  key={user.id}
+                                  {...user}
+                              />
+                          ))
+                      }
+                  </div>
+              )
+          }
+      </main>
   );
 }
